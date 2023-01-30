66981f6ab38531b6835208dc82a07a8fe3d83ec6
Magn3tism <chapagainsakshyam@gmail.com>
2023-01-30 07:46:58 +0545

    Add filter to phonebook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: 1234 },
+    { name: "Asterius", number: 34 },
+    { name: "Pan", number: 124 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setNewFilter] = useState("");
+  const [filteredPeople, setFiltredPeople] = useState(persons);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+    let filterLength = event.target.value.length;
+
+    const newArray = persons.filter(
+      (person) =>
+        person.name.substring(0, filterLength).toUpperCase() ===
+        event.target.value.toUpperCase()
+    );
+
+    setFiltredPeople(newArray);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    if (!newName || !newNumber) return;
 
     let exists = false;
 
@@ -22,17 +48,31 @@ const App = () => {
 
     if (exists) return;
 
-    setPersons(persons.concat({ name: newName }));
+    let newPersons = persons.concat({ name: newName, number: newNumber });
+
+    setPersons(newPersons);
+    setFiltredPeople(newPersons);
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
+      <h2>Search</h2>
+
+      <p>Filter Name</p>
+      <input type="text" value={filter} onChange={handleFilterChange} />
+
       <h2>Phonebook</h2>
 
       <form onSubmit={handleSubmit}>
         <div>
-          Name: <input value={newName} onChange={handleNameChange} />
+          Name:{" "}
+          <input type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          Number:{" "}
+          <input type="text" value={newNumber} onChange={handleNumberChange} />
         </div>
 
         <div>
@@ -42,8 +82,10 @@ const App = () => {
 
       <h2>Numbers</h2>
 
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+      {filteredPeople.map((person) => (
+        <p key={person.number}>
+          {person.name} - {person.number}
+        </p>
       ))}
     </div>
   );
