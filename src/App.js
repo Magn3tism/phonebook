814c94b796243bf573dfb41a2bf0e4c73ad811f0
Magn3tism <chapagainsakshyam@gmail.com>
2023-01-30 07:23:52 +0545

    Complete adding and displaying name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
   return (
-    <div className="App">
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
